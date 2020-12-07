b3ccf5e5345c130cdf5b3d1a99b8176e2a08c60c
limayamil <yamillues@gmail.com>
2020-12-07 17:37:40 -0300

    - incorporado fetch de gifs tras la búsqueda

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 const key = "qg9B328JuyVC0ozHU262REA1CkidGGzJ";
-const giphyTrendingGIFsAPI = "https://api.giphy.com/v1/gifs/trending?api_key=" + key;
+const giphyTrendingGIFs = "https://api.giphy.com/v1/gifs/trending?api_key=" + key;
 const giphyTrendingSearchTerms = "https://api.giphy.com/v1/trending/searches?api_key=" + key;
 const giphySearchSuggestions = "https://api.giphy.com/v1/gifs/search/tags?api_key=" + key + "&q=";
+const giphySearchGIFs = "https://api.giphy.com/v1/gifs/search?api_key=" + key + "&q=";
 let trendingGIFsDiv = document.getElementById("section-tre-car");
 let trendingTopicsDiv = document.getElementById("header-tre-top");
+let formSearch = document.getElementById("form-search");
 let searchBar = document.getElementById("search-bar");
 let searchBarCross = document.getElementById("search-bar-cross");
 let searchSuggestionsList = document.getElementById("search-sug-list");
+let searchResults = document.getElementById("section-res");
+let searchResultsGallery = document.getElementById("section-res-gal");
+let searchResultsTitle = document.getElementById("section-res-tit");
 
 const fetchTrendingGIFs = (giphyAPI) => {
     fetch(giphyAPI)
@@ -44,6 +49,9 @@ const fetchTrendingSearchTerms = (giphyAPI) => {
     });
 }
 
+const searchSuggestion = searchTerm => {
+    alert(searchTerm);
+}
 
 const fetchSearchSuggestions = (giphyAPI, searchTerm) => {
     fetch(giphyAPI + searchTerm)
@@ -51,17 +59,46 @@ const fetchSearchSuggestions = (giphyAPI, searchTerm) => {
         return response.json();
         })
         .then((json) => {
-        console.log(json);
+        //console.log(json);
         searchSuggestionsList.innerHTML = "";
         for (i = 0; i < json.data.length; i++){
             let suggestionItem = document.createElement("li");
-            suggestionItem.innerHTML = '<i class="fas fa-search"></i>' + json.data[i].name;
+            let searchSuggestionFetched = json.data[i].name;
+            suggestionItem.innerHTML = '<i class="fas fa-search"></i>' + searchSuggestionFetched;
             searchSuggestionsList.appendChild(suggestionItem);
+
+            const clickSearchSuggestion = () => {
+                searchSuggestion(searchSuggestionFetched);
+                suggestionItem.removeEventListener("click", clickSearchSuggestion);
+            }
+
+            suggestionItem.addEventListener('click', clickSearchSuggestion);
         }
     });
 }
 
-fetchTrendingGIFs(giphyTrendingGIFsAPI);
+const fetchSearchGIFs = (giphyAPI, searchTerm) => {
+    fetch(giphyAPI + searchTerm)
+        .then((response) => {
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            searchSuggestionsList.innerHTML = "";
+            searchResults.classList.remove("hide");
+            for (i = 0; i < 12; i++){
+                let divGif = document.createElement("div");
+                let gif = document.createElement("img");
+                divGif.classList.add('fetched-gif');
+                gif.src = json.data[i].images.downsized.url;
+                console.log(gif);
+                divGif.appendChild(gif);
+                searchResultsGallery.appendChild(divGif);
+            }
+    });
+}
+
+fetchTrendingGIFs(giphyTrendingGIFs);
 fetchTrendingSearchTerms(giphyTrendingSearchTerms);
 
 const changeSearchBar = () => {
@@ -81,5 +118,16 @@ const clearSearchBar = () => {
     searchSuggestionsList.innerHTML = "";
 }
 
+const showResults = () => {
+    let searchTerm = searchBar.value;
+    fetchSearchGIFs(giphySearchGIFs, searchTerm);
+    searchResultsTitle.textContent = searchTerm;
+}
+
 searchBar.addEventListener('input', changeSearchBar);
 searchBarCross.addEventListener('click', clearSearchBar);
+formSearch.addEventListener('submit', e => {
+    e.preventDefault;
+    e.stopPropagation;
+    showResults();
+});
